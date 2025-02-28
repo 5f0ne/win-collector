54ed8a958f6b0fed9376ca812a1c3ed16a49d021
5f0ne <96919731+5f0ne@users.noreply.github.com>
2025-02-28 09:27:25 +0100

    WMI root/Default namespace added for filter/consumer persistence

--- win-collector-ng.ps1
+++ win-collector-ng.ps1
@@ -226,15 +226,18 @@ Get-ChildItem -Path $psHistoryPath -Force | ForEach-Object {
 
 # WMI Event Filter and Consumer
 $p = Get-FilePath -Path $currentPathPsDir -FileName "wmi-event-filter.csv"
-Get-WMIObject -Namespace root\Subscription -Class __EventFilter | Select-Object Name, EventNamespace, Query, QueryLanguage  | Export-Csv $p -NoTypeInformation
+Get-WMIObject -Namespace root\Subscription -Class __EventFilter | Select-Object __NAMESPACE, Name, EventNamespace, Query, QueryLanguage  | Export-Csv $p -NoTypeInformation
+Get-WMIObject -Namespace root\Default -Class __EventFilter | Select-Object __NAMESPACE, Name, EventNamespace, Query, QueryLanguage  | Export-Csv -Append $p
 
 $p = Get-FilePath -Path $currentPathPsDir -FileName "wmi-event-consumer.csv"
 Get-WMIObject -Namespace root\Subscription -Class __EventConsumer | 
-    Select-Object Name, SourceName, CommandLineTemplate, ExecutablePath, ScriptFilename, ScriptingEngine, ScriptText | 
-    Export-Csv $p -NoTypeInformation
+    Select-Object __NAMESPACE, Name, SourceName, CommandLineTemplate, ExecutablePath, ScriptFilename, ScriptingEngine, ScriptText | Export-Csv $p -NoTypeInformation
+Get-WMIObject -Namespace root\Default -Class __EventConsumer | 
+    Select-Object __NAMESPACE, Name, SourceName, CommandLineTemplate, ExecutablePath, ScriptFilename, ScriptingEngine, ScriptText | Export-Csv -Append $p
 
 $p = Get-FilePath -Path $currentPathPsDir -FileName "wmi-event-filter-consumer-binding.csv"
-Get-WMIObject -Namespace root\Subscription -Class __FilterToConsumerBinding | Select-Object Filter, Consumer | Export-Csv $p -NoTypeInformation
+Get-WMIObject -Namespace root\Subscription -Class __FilterToConsumerBinding | Select-Object __NAMESPACE, Filter, Consumer | Export-Csv $p -NoTypeInformation
+Get-WMIObject -Namespace root\Default -Class __FilterToConsumerBinding | Select-Object __NAMESPACE, Filter, Consumer | Export-Csv -Append $p
 
 # ----------------------------------------------------------------------------------------------------------
 
